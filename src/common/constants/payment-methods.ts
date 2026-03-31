f61ab04e160af54bdd75410713961602bfa4a8e6
Osagie <cj16521>
2026-03-31 13:11:37 +0300

    clean up

--- src/common/constants/payment-methods.ts
+++ src/common/constants/payment-methods.ts
@@ -3,7 +3,6 @@ import { PaymentMethod } from '@prisma/client';
 /** Payment methods that can be used for admin/in-person subscription creation and CSV imports. */
 export const ADMIN_PAYMENT_METHODS = [
   PaymentMethod.MOBILE_MONEY_IN_PERSON,
-  PaymentMethod.BANK_TRANSFER,
   PaymentMethod.BANK_TRANSFER_IN_PERSON,
   PaymentMethod.CARD_IN_PERSON,
   PaymentMethod.COMPLIMENTARY,
